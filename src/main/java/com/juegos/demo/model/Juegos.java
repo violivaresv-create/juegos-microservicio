0b516f7f7e39a16e449922e7039953ac8a565b67
violivaresv-create <vi.olivaresv@duocuc.cl>
2026-04-21 14:58:57 -0400

    Define Juegos class with game attributes
    Added fields for game attributes including name, developer, release date, genre, description, and price.

--- src/main/java/com/juegos/demo/model/Juegos.java
+++ src/main/java/com/juegos/demo/model/Juegos.java
@@ -1,5 +1,39 @@
 package com.juegos.demo.model;
 
-public class Juegos {
+import java.sql.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Juegos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String desarrollador;
+
+    @Column(nullable = false)
+    private Date fechaPublicacion;
+
+    @Column(nullable = false)
+    private String genero;
+
+    @Column(nullable = false)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private int precio;
+    
 }
